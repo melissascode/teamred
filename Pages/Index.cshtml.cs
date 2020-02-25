807c3ff58b73d0e335ccb322444b9019f07d6d29
jnemit2 <jnemit2@students.towson.edu>
2020-02-24 20:16:37 -0500

    Basic login & css

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace teamred.Pages
+namespace project412.Pages
 {
     public class IndexModel : PageModel
     {
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace teamred
+namespace project412
 {
     public class Startup
     {
@@ -24,6 +24,7 @@ namespace teamred
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
